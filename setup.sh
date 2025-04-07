c0abc619956d980756e773e62c868440b468c8f8
lakeninter <lakeninter9@gmail.com>
2025-04-07 13:27:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+# Define color variables
+NC='\033[0m'          # No Color / Reset
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
 # Greetings
-greet="Welcome to CTS!"
-echo $greet
+function greetFunc(){
+    clear
+    echo -e "${BLUE}************************************${NC}"
+    echo -e "${BLUE}*                                  *${NC}"
+    echo -e "${GREEN}* ${BOLD}Welcome To Crazt Tech Solution🔥 *${NC}"
+    echo -e "${BLUE}*                                  *${NC}"
+    echo -e "${BLUE}************************************${NC}"
+
+}
+greetFunc
